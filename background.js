ae8395e4c4df25bac171a9ed282c71d32db5c315
JWK384665d <113614112+JWK384665d@users.noreply.github.com>
2023-01-09 00:01:16 +0900

    backgroundImage

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
+const body = document.body;
+
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-bgImage.src = `image/${chosenImage}`;
 
+bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add("bgImage");
 document.body.appendChild(bgImage);
--- quotes.js
+++ quotes.js
@@ -1,49 +1,31 @@
 const quotes = [
   {
-    quote:
-      "We're gonna choose the blue pill. We're gonna close the curtains. We're gonna rabbit hole down, third act love now.",
-    author: "Bastille",
+    quote: "It is better to fail in originality than to succeed in imitation.",
+    author: "Herman Melville",
   },
   {
     quote:
-      "All of your flaws and all of my flaws, they lie there hand in hand. Ones we've herited, ones that we learnd they pass from man to man",
-    author: "Bastille",
+      "The road to success and the road to failure are almost exactly the same.",
+    author: "Colin R. Davis",
   },
   {
-    quote: "We are the leaders of the not-coming-back's.",
-    author: "5 Seconds of Summer",
-  },
-  {
-    quote:
-      "Blood on my shirt, rose in my hand. Blood on my shirt, heart in my hand Still beating.",
-    author: "5 Seconds of Summer",
-  },
-  {
-    quote: "It's me, Hi! I'm the problem it's me.",
-    author: "Taylor Swift",
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
   },
   {
     quote:
-      "Best believe I'm still bejeweled. When I walk in the room, I can still make the whole place shimmer.",
-    author: "Taylor Swift",
-  },
-  {
-    quote: "Now is not the time to cry, now's the time to find out why.",
-    author: "Oasis",
+      "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
   },
   {
     quote:
-      "Now it's over, can't forget what you said. And I never, wanna do this again. Heartbreaker!",
-    author: "Simple plan",
-  },
-  {
-    quote: "난 땅에서도 숨을 쉴 수 있는 물고기였을지도 몰라",
-    author: "Yerin Back",
+      "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer",
   },
   {
     quote:
-      "I wanna be somewhere like no need to be clear, no need to be explaining. I just wanna rest, no complain, no watching.",
-    author: "Yerin Back",
+      "Success is walking from failure to failure with no loss of enthusiasm.",
+    author: "Winston Churchill",
   },
 ];
 
